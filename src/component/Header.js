27d39392f33fc07f0e973b5584cb16fac420f337
frank <frank@imafidonfrank2015@gmail>
2025-07-11 13:24:06 -0700

    first commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,96 +1,49 @@
 import Form from 'react-bootstrap/Form'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, NavDropdown } from 'react-bootstrap'
 
 const Header = () => {
     return(
-        <Container>
-        <Row>
-            <Col>
-                <Navbar expand="lg" style={{}} class="navbar navbar-expand-sm bg-light navbar-light">
-                        <Navbar.Brand>
-                            <h4 style={{
-                                    color: "#0096ff",
-                                    fontFamily: "Mulish",
-                                    fontSize: 40,
-                                    fontWeight: 800,
-                                    fontStyle: "normal"
-                                }}>
-                                Delsu online
-                            </h4>
-                        </Navbar.Brand>
-                         <Nav className="">
-                            <Nav.Item>
-                                <Nav.Link style={{
-                                    fontSize: 20,
-                                    fontFamily: "Inria Serif",
-                                    fontWeight: 400,
-                                    fontStyle: "normal"
-
-                                }}>Courses</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link style={{
-                                    fontSize: 20,
-                                    fontFamily: "Inria Serif",
-                                    fontWeight: 400,
-                                    fontStyle: "normal"
-                                }}>Lectures</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Form className="mr-auto">
-                                    <Form.Group>
-                                        <Form.Control
-                                            placeholder="Search courses"
-                                            type="text"
-                                            className="outline-primary"
-                                            style={{
-                                                width: 400,
-                                                borderRadius: 20,
-                                                fontSize: 20,
-                                                fontFamily: "Inria Serif",
-                                                fontWeight: 400,
-                                                fontStyle: "normal"
-                                            }}
-                                        />
-                                    </Form.Group>
-                                </Form>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link 
-                                    style={{
-                                        fontSize: 20,
-                                        fontFamily: "Inria Serif",
-                                        fontWeight: 400,
-                                        fontStyle: "normal"
-                                    }} 
-                                    href="login"
-                                >
-                                    Log in
-                                    </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Button 
-                                    style={{
-                                        fontSize: 20,
-                                        fontFamily: "Inria Serif",
-                                        fontWeight: 400,
-                                        fontStyle: "normal"
-                                    }}  
-                                    className="lg" 
-                                    variant="outline-primary"
-                                >
-                                    Sign up
-                                </Button>
-                            </Nav.Item>
-                           
-                        </Nav>
-
-                </Navbar>
-            </Col>
-        </Row>
-        </Container>
+       <Navbar expand="lg" className="bg-body-light">
+      <Container>
+        <Navbar.Brand href="#">
+            <h4 style={{color: '#0096ff', fontFamily: 'Mulish', fontSize: 35, fontWeight: 800, fontStyle: 'normal'}}>
+                Delsu Online
+            </h4>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1" style={{fontSize: 16, fontFamily: 'Inria Serif', fontWeight: 400, fontStyle: "normal"}}>Our Courses</Nav.Link>
+            <Nav.Link href="#action2" style={{fontSize: 16, fontFamily: 'Inria Serif', fontWeight: 400, fontStyle: "normal"}}>Join Us</Nav.Link>
+            <NavDropdown style={{fontSize: 16, fontFamily: 'Inria Serif', fontWeight: 400, fontStyle: "normal"}} title="Latest Courses" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+          
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button style={{color: '#fff', backgroundColor: '#0096ff'}} variant="outline-primary">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     )
 }
 export default Header
