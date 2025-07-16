b6698875404becffd789bb13accf7825aab1bc8f
frank <frank@imafidonfrank2015@gmail>
2025-07-16 12:41:01 -0700

    first commit

--- src/component/PopularCourses.js
+++ src/component/PopularCourses.js
@@ -14,17 +14,75 @@ const PopularCourses = ({mobile}) => {
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 3
+            items: 4
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
-            items: 2
+            items: 4
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
             items: 1
         }
     };
+
+    const all_courses = [
+        {
+            id: "1",
+            image: img2,
+            title: "GST 101",
+            text: "Some quick example text to build on the card title and make up the bulk of the cards content.",
+        },
+        {
+            id: "2",
+            image: img2,
+            title: "GST 102",
+            text: "Some quick example text to build on the card title and make up the bulk of the cards content.",
+        },
+        {
+            id: "3",
+            image: img2,
+            title: "GST 103",
+            text: "Some quick example text to build on the card title and make up the bulk of the cards content.",
+        },
+        {
+            id: "4",
+            image: img2,
+            title: "GST 104",
+            text: "Some quick example text to build on the card title and make up the bulk of the cards content.",
+        },
+        {
+            id: "5",
+            image: img2,
+            title: "GST 105",
+            text: "Some quick example text to build on the card title and make up the bulk of the cards content.",
+        },
+        {
+            id: "6",
+            image: img2,
+            title: "GST 106",
+            text: "Some quick example text to build on the card title and make up the bulk of the cards content.",
+        },
+    ]
+
+    let courses
+    courses = all_courses.map(m => (
+        <div style={{margin: 10}}>
+            <Card className="bg-body-tertiary">
+                <Card.Img variant="top" src={m.image} />
+                    <Card.Body>
+                        <Card.Title style={{fontSize: 20, fontWeight: 700, fontStyle: "normal", fontFamily: "mullin"}}>{m.title}</Card.Title>
+                        <Card.Text style={{fontSize: 16, fontWeight: 400, fontStyle: "normal", fontFamily: "Inria Serif"}}>
+                            {m.text}
+                        </Card.Text>
+                        <Button style={{backgroundColor: '#0096ff', fontWeight: 400, fontStyle: 'normal', fontFamily: "Inria Serif", fontSize: 16}}>
+                            <BiSolidVideo color="#fff" />&nbsp;
+                             View watch
+                        </Button>
+                    </Card.Body>
+            </Card>
+        </div>
+    ))
 
     return(
         <div style={{paddingTop: 50, paddingBottom: 50}} className="">
@@ -34,76 +92,9 @@ const PopularCourses = ({mobile}) => {
                         <h4 style={{fontSize: 35, fontStyle: "normal", fontWeight: 700, fontFamily: "Mullin", color: "#000"}} className="text-center">General courses</h4>
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
-                        <Carousel responsive={responsive}>
-                            <div>
-                                <Card className="bg-body-tertiary">
-                                    <Card.Img variant="top" src={img2} />
-                                    <Card.Body>
-                                        <Card.Title style={{fontSize: 20, fontWeight: 700, fontStyle: "normal", fontFamily: "mullin"}}>GST 101</Card.Title>
-                                        <Card.Text style={{fontSize: 16, fontWeight: 400, fontStyle: "normal", fontFamily: "Inria Serif"}}>
-                                            Some quick example text to build on the card title and make up the
-                                            bulk of the card's content.
-                                        </Card.Text>
-                                        <Button style={{backgroundColor: '#0096ff', fontWeight: 400, fontStyle: 'normal', fontFamily: "Inria Serif", fontSize: 16}}>
-                                            <BiSolidVideo color="#fff" />&nbsp;
-                                            View watch
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                            <div>
-                                <Card className="bg-body-tertiary">
-                                    <Card.Img variant="top" src={img2} />
-                                    <Card.Body>
-                                        <Card.Title style={{fontSize: 20, fontWeight: 700, fontStyle: "normal", fontFamily: "mullin"}}>GST 102</Card.Title>
-                                        <Card.Text style={{fontSize: 16, fontWeight: 400, fontStyle: "normal", fontFamily: "Inria Serif"}}>
-                                            Some quick example text to build on the card title and make up the
-                                            bulk of the card's content.
-                                        </Card.Text>
-                                        <Button style={{backgroundColor: '#0096ff', fontWeight: 400, fontStyle: 'normal', fontFamily: "Inria Serif", fontSize: 16}}>
-                                            <BiSolidVideo color="#fff" />&nbsp;
-                                            View watch
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                            <div>
-                                <Card className="bg-body-tertiary">
-                                    <Card.Img variant="top" src={img2} />
-                                    <Card.Body>
-                                        <Card.Title style={{fontSize: 20, fontWeight: 700, fontStyle: "normal", fontFamily: "mullin"}}>GST 103</Card.Title>
-                                        <Card.Text style={{fontSize: 16, fontWeight: 400, fontStyle: "normal", fontFamily: "Inria Serif"}}>
-                                            Some quick example text to build on the card title and make up the
-                                            bulk of the card's content.
-                                        </Card.Text>
-                                        <Button style={{backgroundColor: '#0096ff', fontWeight: 400, fontStyle: 'normal', fontFamily: "Inria Serif", fontSize: 16}}>
-                                            <BiSolidVideo color="#fff" />&nbsp;
-                                            View watch
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                            <div>
-                                <Card className="bg-body-tertiary">
-                                    <Card.Img variant="top" src={img2} />
-                                    <Card.Body>
-                                        <Card.Title style={{fontSize: 20, fontWeight: 700, fontStyle: "normal", fontFamily: "mullin"}}>GST 104</Card.Title>
-                                        <Card.Text style={{fontSize: 16, fontWeight: 400, fontStyle: "normal", fontFamily: "Inria Serif"}}>
-                                            Some quick example text to build on the card title and make up the
-                                            bulk of the card's content.
-                                        </Card.Text>
-                                        <Button style={{backgroundColor: '#0096ff', fontWeight: 400, fontStyle: 'normal', fontFamily: "Inria Serif", fontSize: 16}}>
-                                            <BiSolidVideo color="#fff" />&nbsp;
-                                            View watch
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                        </Carousel>;
-                    </Col>
-                </Row>
+                    <Carousel responsive={responsive}>
+                        {courses}
+                    </Carousel>; 
             </Container>
         </div>
     )
